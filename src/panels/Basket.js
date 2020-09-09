0bd57cfbc731c5b844f9be2a6446d12e6d5d7fd5
xkrutoix <xkrutoix@gmail.com>
2020-09-09 18:36:10 +0300

    fix bug with invalid time input

--- src/panels/Basket.js
+++ src/panels/Basket.js
@@ -6,6 +6,7 @@ import Checkbox from './Checkbox';
 
 import edit from '../img/edit.svg';
 import './place.css';
+import {isValid} from "../utils/isValid";
 
 
 const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
@@ -107,8 +108,8 @@ const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
         <h3>Время:</h3>
         <div className="Place__choice-item">
           <span>Как можно быстрее</span>
-          <Checkbox 
-            checked={faster} 
+          <Checkbox
+            checked={faster}
             onToggle={() => {
               if (faster) {
                 setFaster(false);
@@ -122,6 +123,7 @@ const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
         <div className="Place__choice-item">
           <span>Назначить</span>
           <input
+            className={isValid(time) || faster ? undefined : 'invalid-time'}
             value={time}
             onFocus={() => {
               setFaster(false);
@@ -146,7 +148,7 @@ const Basket = ({ match: { params: { areaId, itemId }}, foodAreas, order }) => {
           <Checkbox checked={!selfService} onToggle={() => setSelfService(!setSelfService)} />
         </div>
       </div>
-      <footer className="Place__footer">
+      <footer className={isValid(time) || faster ? "Place__footer" : "Place__footer disable-link"}>
         <Link to={`/order/${area.id}/${item.id}`} className="Place__order">
           Оплатить {price}
         </Link>
